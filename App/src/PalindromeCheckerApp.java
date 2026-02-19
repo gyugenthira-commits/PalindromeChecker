fcfdeb044227803169246151028ab3e9c645a2a1
gyugenthira-commits <gyugenthira@gmail.com>
2026-02-19 10:47:50 +0530

    Writing Palindrome Checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
     static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome Checker Management System\nVersion : 1.0\nSystem initialized successfully.");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String input=sc.nextLine();
+        int length=input.length();
+        boolean isPalindrome = true;
+        for(int i=0;i<length/2;i++) {
+            if (input.charAt(i) != input.charAt(length-i-1)) {
+                isPalindrome=false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println("It is a Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+        sc.close();
     }
 }
