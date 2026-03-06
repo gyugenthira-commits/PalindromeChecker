183609f616d174d98a9ef143701ea6c17e03bd07
gyugenthira-commits <gyugenthira@gmail.com>
2026-03-06 09:32:42 +0530

    UC12 Strategy pattern for palindrome algorithm

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,18 +1,26 @@
 import java.util.Scanner;
+import java.util.Stack;
 
-class PalindromeChecker {
+interface PalindromeStrategy {
+    boolean check(String input);
+}
 
-    public boolean checkPalindrome(String input) {
-        int start = 0;
-        int end = input.length() - 1;
+class StackStrategy implements PalindromeStrategy {
 
-        while (start < end) {
-            if (input.charAt(start) != input.charAt(end)) {
+    public boolean check(String input) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 return false;
             }
-            start++;
-            end--;
         }
+
         return true;
     }
 }
@@ -25,8 +33,9 @@ public class PalindromeCheckerApp {
         System.out.print("Input: ");
         String input = sc.nextLine();
 
-        PalindromeChecker checker = new PalindromeChecker();
-        boolean result = checker.checkPalindrome(input);
+        PalindromeStrategy strategy = new StackStrategy();
+
+        boolean result = strategy.check(input);
 
         System.out.println("Is Palindrome?: " + result);
     }
